0239da2ec2406ad34a89005c74ff05cc6df74ee0
VED TALMALEY <vedwork21@gmail.com>
2026-04-26 00:43:34 +0530

    Time: 10 ms (10.48%), Space: 23.6 MB (13.26%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        string result;
+        while (ss>>word)
+        result=word + (result.empty() ? "" : " ") + result;
+
+    
+    return result;
+    }
+};
