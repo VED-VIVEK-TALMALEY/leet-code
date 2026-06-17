240e827cc4d963fc4e417d0ac60b07cebf6771db
VED TALMALEY <vedwork21@gmail.com>
2026-06-17 12:54:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x<0)
+        if (x<0||(x%10==0&&x!=0))
         return false ;
-        if (x>=0&&x<10)
-        return true;
-
-        vector<int>result;
-       int digit=0;
-       int count =0;
-while (x > 0){
-    digit = x % 10 ;
-    x/=10;
- 
-    result.push_back(digit);
-}
-//reverse(result.begin(),result.end());
-for(int i = 0 ; i < result.size()/2 ; i++){
-      if(result[i]!=result[result.size()-i-1]){ 
-      return false ; 
-      }
+        long long int r=0;
+        while(x>r){
+          r=(r*10)+(x%10);
+        x=x/10;    
     
-} 
-return true;
+      }
+    return(r==x||x==r/10);
     }
 };
