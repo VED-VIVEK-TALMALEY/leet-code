64618846dcba30ad08e5bebb7ad7d9f9795c104a
VED TALMALEY <vedwork21@gmail.com>
2026-06-22 22:03:16 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.4%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,12 +1,11 @@
-#include <cmath>
+#include<cmath>
 #include <iostream>
-
-using namespace std; // Corrected syntax
-
+#include <algorithm>
+using namespace std ;
 class Solution {
+
 public:
     int mySqrt(int x) {
-        
-        return static_cast<int>(floor(sqrt(x))); 
+        return static_cast <int>(floor(sqrt(x)));
     }
 };
