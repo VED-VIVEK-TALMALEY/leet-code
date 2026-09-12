326fa66fade1923ebf877d779c899d8a4796b9d6
VED TALMALEY <vedwork21@gmail.com>
2026-09-12 15:56:16 +0530

    Time: 31 ms (23.1%), Space: 37.6 MB (23.08%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -5,18 +5,17 @@ public:
         unordered_set<int> set2(nums2.begin(), nums2.end());
         
         vector<int> diff1, diff2;
-        diff1.reserve(set1.size());
-diff2.reserve(set2.size());
+        
         for (int num : set1) {
-            if (set2.find(num) == set2.end()) {
+            if (set2.count(num) == 0) {
                 diff1.push_back(num);
+            } else {
+                set2.erase(num); 
             }
         }
         
         for (int num : set2) {
-            if (set1.find(num) == set1.end()) {
-                diff2.push_back(num);
-            }
+            diff2.push_back(num);
         }
         
         return {diff1, diff2};
